4df9ee952fa4b0c818f4607ec686b706b3d2d4ef
Shibuya Naoki <naoki.shibuya@jp.sony.com>
2013-12-09 23:55:15 +0900

    add DB access skeleton

--- routes/login.js
+++ routes/login.js
@@ -3,11 +3,23 @@
  * Login and Register
  */
 
+var mysql    = require('mysql');
+var settings = require('./../settings.json');
 
+// create db connection as pooled connection
+var pool = mysql.createPool({
+  host     : settings.dbhost,
+  port     : settings.dbport,
+  user     : settings.dbuser,
+  password : settings.dbpassword
+});
+
+var SQLselect = "select * from webgame.User WHERE name = ? AND password = ?";
+var SQLinsert = "insert into webgame.User (email, name, password, icon_url) VALUES(?, ?, ?, ?)";
 
 function login(req, res, next) {
   console.log(req.body);
-  
+
   var submit;
   if( req.body.submit ) {
     submit = req.body.submit;
@@ -22,12 +34,11 @@ function login(req, res, next) {
   else {
     console.log("ERROR!");
   }
-
+  
 };
 
 function _register(req, res, next) {
   console.log("Register!");
-  console.log(req.body);
 
   var mail;
   var name;
@@ -43,13 +54,21 @@ function _register(req, res, next) {
     pass = req.body.pass;
   }
   
+  // DB connection
+  pool.getConnection( function( err, connection ) {
+    // Use the connection
+    connection.query( SQLinsert, [mail, name, pass, ''], function( err, rows ) {
+      connection.release();
+    } );
+  } );
+
+  
   res.render('index', { login_message: '', register_message: 'mail:' + mail + ', name:' + name + ', pass:' + pass });
 
 };
 
 function _login(req, res, next) {
   console.log("Login!");
-  console.log(req.body);
 
   var name;
   var pass;
@@ -60,6 +79,21 @@ function _login(req, res, next) {
   if( req.body.pass ) {
     pass = req.body.pass;
   }
+
+  // DB connection
+  pool.getConnection( function( err, connection ) {
+    // Use the connection
+    connection.query( SQLselect, [name, pass], function( err, rows ) {
+      if( rows === null || rows === undefined || rows.length === 0 ) {
+        console.log( 'login error!' );
+      }
+      else {
+        console.log( 'login success!' );
+      }
+      connection.release();
+    } );
+  } );
+
 
   res.render('index', { login_message: 'name:' + name + ', pass:' + pass, register_message: '' });
 
